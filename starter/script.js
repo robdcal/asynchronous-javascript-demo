d0496f688d1644c335aae4b0e8f6e2e94340ce6b
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-04-24 15:25:59 +0100

    using Promise.race and Promise.allSettled

--- starter/script.js
+++ starter/script.js
@@ -411,17 +411,47 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
     })
 }
 
-const get3Countries = async function (c1, c2, c3) {
-    try {
-        const data = await Promise.all([
-            getJSON(`https://restcountries.com/v2/name/${c1}/`),
-            getJSON(`https://restcountries.com/v2/name/${c2}/`),
-            getJSON(`https://restcountries.com/v2/name/${c3}/`)
-        ])
-        console.log(data.map(d => d[0].capital))
-    } catch (err) {
-        console.error(err)
-    }
-}
+// const get3Countries = async function (c1, c2, c3) {
+//     try {
+//         const data = await Promise.all([
+//             getJSON(`https://restcountries.com/v2/name/${c1}/`),
+//             getJSON(`https://restcountries.com/v2/name/${c2}/`),
+//             getJSON(`https://restcountries.com/v2/name/${c3}/`)
+//         ])
+//         console.log(data.map(d => d[0].capital))
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
 
-get3Countries('portugal', 'canada', 'tanzania')
+// get3Countries('portugal', 'canada', 'tanzania')
+
+// (async function () {
+//     const res = await Promise.race([
+//         getJSON(`https://restcountries.com/v2/name/italy`),
+//         getJSON(`https://restcountries.com/v2/name/mexico`),
+//         getJSON(`https://restcountries.com/v2/name/egypt`)
+//     ])
+//     console.log(res[0])
+// })()
+
+// const timeout = function (s) {
+//     return new Promise(function (_, reject) {
+//         setTimeout(function () {
+//             reject(new Error('Request took too long!'))
+//         }, sec)
+//     })
+// }
+
+// Promise.race([
+//         getJSON(`https://restcountries.com/v2/name/italy`),
+//         timeout(1)
+//     ])
+//     .then(res => console.log(res[0]))
+//     .catch(err => console.error(err))
+
+Promise.allSettled([
+    Promise.resolve('Success'),
+    Promise.reject('Error'),
+    Promise.resolve('Success')
+]).then(res => console.log(res))
